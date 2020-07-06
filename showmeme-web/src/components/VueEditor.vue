0fcf0c03d3ced8d65ef122df12d6922722035a85
zendarva <zendarva@gmail.com>
2020-07-05 21:42:55 -0500

    Updated comments to use their own service.

---
+++ showmeme-web/src/components/VueEditor.vue
@@ -0,0 +1,47 @@
+<template>
+<v-card>
+  <editor-content class="editor-box px-2" :editor="editor" />
+</v-card>
+</template>
+
+<script>
+  import { Editor, EditorContent, EditorMenuBar  } from 'tiptap';
+
+  export default {
+    components:{
+      TipTapEditor,
+      EditorContent
+    },
+    props:{
+      content: String,
+      promptText: String
+    },
+    data() {
+      return {
+        editor: new Editor({
+          content: this.postBody
+        }),
+      }
+    }
+  }
+
+</script>
+
+<style >
+  .editor-box> * {
+    border-color: grey;
+    border-style: solid;
+    border-width: 1px;
+    padding-left: 5px;
+
+  }
+
+  .is-active{
+    border-color: grey;
+    border-style: solid;
+    border-width: 1px;
+  }
+  /* *:focus {
+     outline: none;
+ }  */
+</style>
